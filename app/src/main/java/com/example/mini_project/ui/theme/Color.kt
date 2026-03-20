dabcee4df4005cb91766d184ee8dbc286d26168e
Tunzta <tn183chilang@gmail.com>
2026-03-20 14:52:30 +0700

    Tính năng: lọc và xoá

--- app/src/main/java/com/example/mini_project/ui/theme/Color.kt
+++ app/src/main/java/com/example/mini_project/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val RoomOccupied = Color(0xFFEF5350)   // Đỏ - đang thuê
+val RoomAvailable = Color(0xFF66BB6A)   // Xanh - trống
